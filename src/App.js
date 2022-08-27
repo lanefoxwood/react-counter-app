3376b836d69382c7903c557aa3ca776c1d579cde
lanefoxwood <lanefoxwood@gmail.com>
2022-08-27 13:41:39 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+
 
 function App() {
+
+const [count, setCount] = useState(4)
+const [theme, setTheme] = useState('blue')
+
+function decrementCount(){
+  setCount(prevCount => prevCount - 1)
+  setTheme('blue')
+}
+
+
+function incrementCount() {
+  setCount(prevCount => prevCount + 1)
+  setTheme('red')
+}
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <button onClick={decrementCount}>-</button>
+      <span>{count}</span>
+      <span>{theme}</span>
+      <button onClick={incrementCount}>+</button>
+    </>
   );
 }
 
